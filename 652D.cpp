c84616e11c7951f3cb2dd629ff0ee82361570d7e
Cao Thanh Hieu <159222107+hiukaochan@users.noreply.github.com>
2024-06-27 18:26:18 +0700

    Update 652D.cpp

--- 652D.cpp
+++ 652D.cpp
@@ -1,17 +1,26 @@
-//#include <bits/stdc++.h>
-#include <iostream>
-#include <algorithm>
+#include <bits/stdc++.h>
 #define maxn 200001
-#define maxxy 32001
-
+ 
 using namespace std;
-
+ 
 int bit_sum[maxn], ans[maxn];
-
+ 
+struct pii {
+    int x, y, id;
+} a[maxn];
+ 
+bool cmp_y(pii a, pii b) {
+    return (a.y < b.y);
+}
+ 
+bool cmp_x(pii a, pii b) {
+    return (a.x > b.x || (a.x == b.x && a.y < b.y));
+}
+ 
 int low_bit(int t) {
     return t&(-t);
 }
-
+ 
 int sum(int t) {
     int sum = 0;
     while (t > 0){
@@ -20,27 +29,36 @@ int sum(int t) {
     }
     return sum;
 }
-
-void add(int t){
-    while (t <= maxxy)
+ 
+void add(int t, int n){
+    while (t <= n)
     {
         bit_sum[t]++;
         t += low_bit(t);
     }
 }
-
+ 
 int main()
 {
-    int n,x,y;
+    int n;
     cin >> n;
-    for (int i = 0; i < n; ++i)
+    for (int i = 1; i <= n; ++i)
     {
-        cin >> x >> y;
-        x++;
-        ans[sum(x)]++;
-        add(x);
+        cin >> a[i].x >> a[i].y;
+        a[i].id = i;
     }
-    for (int i = 0; i < n; ++i)
+    sort(a+1,a+1+n,cmp_y);
+    for (int i = 1; i <= n; ++i)
+    {
+        a[i].y = i;
+    }
+    sort(a+1,a+1+n,cmp_x);
+    for (int i = 1; i <= n; ++i)
+    {
+        ans[a[i].id] = sum(a[i].y);
+        add(a[i].y,n);
+    }
+    for (int i = 1; i <= n; ++i)
         cout << ans[i] << "\n";
     return 0;
 }
